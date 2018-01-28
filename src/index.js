f37e84fe425e711d9b06e6ecf269dcf931ac7869
TainaLepisto <lepisto.taina@gmail.com>
2018-01-29 00:29:03 +0200

    1.8

--- src/index.js
+++ src/index.js
@@ -1,6 +1,44 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+
+
+// Button vastaa yksittäistä palautteenantonappia
+const Button = ({ handleClick, text }) => (
+    <button onClick={handleClick}>
+      {text}
+    </button>
+)
+
+// Statistics huolehtii tilastojen näyttämisestä
+const Statistics = ({hyva, neutraali, huono, yhteensa}) => {
+    if (yhteensa === 0) {
+        return (
+          <div>
+            <em>Anna palautetta nappeja painelemalla</em>
+          </div>
+        )
+      }
+      return (
+        <div>
+           Hyvä: {hyva} <br/>
+           Neutraali: {neutraali} <br/>
+           Huono: {huono} <br/>
+           <br/>
+           <Statistic hyva={hyva} huono={huono} yhteensa={yhteensa} />
+        </div>
+      )
+}
+
+// Statistic huolehtii yksittäisen tilastorivin, esim. keskiarvon näyttämisestä
+const Statistic = ({hyva, huono, yhteensa}) => (
+    <div>
+        Keskiarvo: { Math.round((hyva + -1*huono ) / yhteensa *10) / 10} <br/>
+        Positiivisia: { Math.round(hyva  / yhteensa * 100 *10) / 10} %
+    </div>
+)
+
+
 class App extends React.Component {
     constructor() {
       super()
@@ -14,7 +52,7 @@ class App extends React.Component {
 
     handler = (palaute) => {
         console.log('nappia painettu ', palaute)
-
+    
         return () => {
             if(palaute==='hyva'){
                 this.setState({ hyva: this.state.hyva +1, yhteensa: this.state.yhteensa +1 })
@@ -27,39 +65,18 @@ class App extends React.Component {
             }
         }
     }
-    
-    historia = () => {
-        if (this.state.yhteensa === 0) {
-          return (
-            <div>
-              <em>Anna palautetta nappeja painelemalla</em>
-            </div>
-          )
-        }
-        return (
-          <div>
-            Hyvä: {this.state.hyva} <br/>
-            Neutraali: {this.state.neutraali} <br/>
-            Huono: {this.state.huono} <br/>
-            <br/>
-            Keskiarvo: { Math.round((this.state.hyva + -1*this.state.huono ) / this.state.yhteensa *10) / 10} <br/>
-            Positiivisia: { Math.round(this.state.hyva  / this.state.yhteensa * 100 *10) / 10} %
-          </div>
-        )
-      }
+
 
     render() {
         return (
             <div>
-            
-            <h1>Anna palautetta</h1>
-            <button onClick={this.handler('hyva')}>Hyvä</button>
-            <button onClick={this.handler('neutraali')}>Neutraali</button>
-            <button onClick={this.handler('huono')}>Huono</button>
+                <h1>Anna palautetta</h1>
+                <Button handleClick={this.handler('hyva')} text="Hyvä" />
+                <Button handleClick={this.handler('neutraali')} text="Neutraali" />
+                <Button handleClick={this.handler('huono')} text="Huono" />
 
-            <h2>Statistiikka</h2>
-            <div>{this.historia()}</div>
-
+                <h2>Statistiikka</h2>
+                <Statistics hyva={this.state.hyva} neutraali={this.state.neutraali} huono={this.state.huono} yhteensa={this.state.yhteensa} />
             </div>
         )
     }
